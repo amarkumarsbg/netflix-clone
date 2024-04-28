ce2b3f9b2f7c5cfba50d9a8720e2b52a5c84f759
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-04-28 12:14:10 +0530

    Home page and title Cards completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,45 @@
 import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+
 import "./Home.css";
+import TitleCards from "../../components/TitleCards/TitleCards";
 
 const Home = () => {
   return (
-    <div className="navbar">
+    <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            fugiat quas modi laboriosam temporibus? Cum, at quam voluptatibus
+            eos non tempora sunt earum odio eligendi assumenda eum maiores
+            doloribus maxime.
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"} />
+        <TitleCards title={"Upcoming"} />
+        <TitleCards title={"Top Pics for You"} />
+      </div>
     </div>
   );
 };
